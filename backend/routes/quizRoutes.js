be26a69535df2ec06dd86166cac26e641c446449
Dhuruv gupta #1 <160989356+dhurv-code@users.noreply.github.com>
2026-01-18 13:36:36 +0530

    Update quizRoutes.js

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -4,7 +4,7 @@ const auth = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-// Create quiz
+
 router.post("/create", auth, async (req, res) => {
   const quiz = await Quiz.create({
     ...req.body,
@@ -13,19 +13,16 @@ router.post("/create", auth, async (req, res) => {
   res.json(quiz);
 });
 
-// Get all quizzes
 router.get("/all", async (req, res) => {
   const quizzes = await Quiz.find().populate("createdBy","email");
   res.json(quizzes);
 });
 
-// Get quiz by ID
 router.get("/:id", async (req, res) => {
   const quiz = await Quiz.findById(req.params.id);
   res.json(quiz);
 });
 
-// Submit quiz
 router.post("/submit/:id", async (req, res) => {
   const { answers } = req.body;
   const quiz = await Quiz.findById(req.params.id);
@@ -53,7 +50,6 @@ router.delete("/:id", auth, async (req, res) => {
       return res.status(404).json({ message: "Quiz not found" });
     }
 
-    // only creator can delete
     if (quiz.createdBy.toString() !== req.user.id) {
       return res.status(403).json({ message: "Not allowed" });
     }
